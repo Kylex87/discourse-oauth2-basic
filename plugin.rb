4e2ac7dd567f55a5bea3640286e463cd6c45ffbe
kylex87 <kylex@web.de>
2016-03-15 03:54:23 -0400

    Fix for after_create_acocunt

--- plugin.rb
+++ plugin.rb
@@ -165,7 +165,7 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
       end
     end
     
-    result.extra_data = { oauth2_basic_user_id: user_details[:user_id] }
+    result.extra_data = { oauth2_basic_user_id: user_details[:user_id], groups: user_details[:groups] }
     if not result.user.nil?
       update_user_groups(result.user, user_details[:groups])
     end
@@ -175,6 +175,8 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
   def after_create_account(user, auth)
     Rails.logger.debug("Called after create account for user "+user.to_s+" and auth: "+auth.to_s)
     ::PluginStore.set("oauth2_basic", "oauth2_basic_user_#{auth[:extra_data][:oauth2_basic_user_id]}", user_id: user.id)
+    groups = auth[:extra_data][:groups]
+    update_user_groups(user, groups)
   end
 end
 auth_provider title_setting: "oauth2_button_title",
